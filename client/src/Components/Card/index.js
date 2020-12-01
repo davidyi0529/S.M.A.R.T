c0c7ed6dfdc1e656009cb34e8839be4afc3fd019
Jefry Romero <romerojefry@icloud.com>
2020-12-01 13:26:16 -0800

    creating a card components

---
+++ client/src/Components/Card/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Card() {
+    return (
+             <div className="col-md-3 mx-5 align-self-center">
+                <div className="card text-dark bg-warning mb-3" styles="max-width: 18rem;">
+                 <div className="card-header">name of stock goes here<button type="button" class="btn btn-outline-danger ml-5">Remove</button></div>
+                  <div className="card-body">
+                   <h5 className="card-title">Stock attribute goes here</h5>
+                   <p className="card-text">this room will be for the stock performances</p>
+                   <button type="button" class="btn btn-dark">Add Watchlist</button>
+                  </div>
+                </div>
+             </div>
+    )
+}
+
+export default Card;
--- client/src/pages/myInvestments.js
+++ client/src/pages/myInvestments.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "../Components/Card"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -15,29 +16,18 @@ function MyInvestments() {
                      You can always remove or add it to the watchlist to keep track and away from your investments
                   </p>
               </div>
-             <div className="col-md-3 mx-5 align-self-center">
-                <div className="card text-dark bg-warning mb-3" styles="max-width: 18rem;">
-                 <div className="card-header">name of stock goes here<button type="button" class="btn btn-outline-danger ml-5">Remove</button></div>
-                  <div className="card-body">
-                   <h5 className="card-title">Stock attribute goes here</h5>
-                   <p className="card-text">this room will be for the stock performances</p>
-                   <button type="button" class="btn btn-dark">Add Watchlist</button>
-                  </div>
-                </div>
-             </div>
            </div>
            <div className="row my-5">
               <div className="col-md-3 offset-md-3">
-              <div className="card text-dark border-warning mb-3" styles="max-width: 18rem;">
-                 <div className="card-header">name of stock goes here<button type="button" class="btn btn-outline-danger ml-5">Remove</button>
-                 </div>
-                  <div className="card-body">
-                   <h5 className="card-title">Stock attribute goes here</h5>
-                   <p className="card-text">this room will be for the stock performances</p>
-                   <button type="button" class="btn btn-dark">Add Watchlist</button>
-                  </div>
-                </div>
+      
               </div>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
            </div>
         </div>
 
