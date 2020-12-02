17aee65ae13fb7449467bc532b166adeff38c86a
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-12-02 09:39:02 -0800

    Revert "corrections to make app work"

--- client/src/Components/Jumbotron/index.js
+++ client/src/Components/Jumbotron/index.js
@@ -3,10 +3,10 @@ import "./style.css";
 
 function Jumbotron() {
     return(
-        <div className="jumbotron jumbotron-fluid">
-  <div className="container">
-    <h1 className="display-4 text-dark">SMART</h1>
-    <p className="lead text-white">Stocks Made Accessible Readable and Tolerable</p>
+        <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4 text-dark">SMART</h1>
+    <p class="lead text-white">Stocks Made Accessible Readable and Tolerable</p>
   </div>
 </div>
     )
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
 const stockRoutes = require("./stocks");
-const userRoutes = require("./user");
 
 // stock routes
 router.use("/stocks", stockRoutes);
-
-router.use("/user", userRoutes)
 
 module.exports = router;
