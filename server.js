ac29ad0fcc0fae8959172b6b956f66978f9b2d41
David Yi <david.yi.0529@gmail.com>
2020-12-02 14:45:35 -0800

    deploying

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/smart-stocks-dy",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/stocks",
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
